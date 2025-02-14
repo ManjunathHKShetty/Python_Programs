ca69f83264e92e1e6bf8e0cb9063b1c63b1fe9ab
ManjunathHKShetty <manjunathhkshetty@gmail.com>
2025-02-14 21:29:27 +0530

    Two Pointers

---
+++ TwoPointers/ReverseString.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseString(self, s):
+        n = len(s)
+        l = 0
+        r = n - 1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+
+X = Solution()
+s_list = ["h","e","l","l","o"]
+s_list2 = ["H","a","n","n","a","h"]
+X.reverseString(s_list)
+print(s_list)
+X.reverseString(s_list2)
+print(s_list2)
