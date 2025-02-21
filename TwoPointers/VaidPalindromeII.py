7f567a23d15478f67e39d1c4c239c9cb6546b670
ManjunathHKShetty <manjunathhkshetty@gmail.com>
2025-02-21 16:31:12 +0530

    Valid Palindrome

---
+++ TwoPointers/VaidPalindromeII.py
@@ -0,0 +1,22 @@
+class solution:
+    def validPalindrome(self, s):
+        n = len(s)
+        l = 0
+        r = n - 1
+
+        while l < r:
+            if s[l] != s[r]:
+                skipL = s[l+1:r+1]
+                skipR = s[l:r]
+                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+
+            l += 1
+            r -= 1
+
+        return True
+
+X = solution()
+result_1 = X.validPalindrome("abca")
+print(result_1)
+result_2 = X.validPalindrome("abc")
+print(result_2)
